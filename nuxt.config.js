f812259ab1295b5177f5c6cfcbe6b70eec1943c6
toki23 <blancshio@gmail.com>
2021-06-18 22:09:01 +0900

    second commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/manabu-Allergies/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'manabu-Allergies',
