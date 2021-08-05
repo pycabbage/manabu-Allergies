13049e1f9d6a56720de24b88f3ab5e768e259052
hanatsu-1404 <m20028@g.metro-cit.ac.jp>
2021-08-05 18:50:40 +0900

    pages/index.vueにあったリンク集をコンポーネント化。

---
+++ components/ContentCollections.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-row justify="left">
+      <v-card tile>
+        <v-list shaped>
+          <v-subheader>{{ CollectionName }}</v-subheader>
+          <v-list-item-group color="primary">
+            <slot name="CollectionContent"/>
+          </v-list-item-group>
+        </v-list>
+      </v-card>
+    </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    CollectionName:String,
+  },
+}
+</script>
