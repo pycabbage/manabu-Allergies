a6d31fdc49af9db8f1eb04002416c0f701d16d10
Marusoftware <marusoftware@outlook.jp>
2021-08-05 10:20:36 +0900

    ランキングページにsortを実装

--- pages/ranking.vue
+++ pages/ranking.vue
@@ -2,7 +2,7 @@
   <v-app id="ranking">
     <h1>ランキング</h1>
     <ol>
-      <li v-for="(data, key) in datas" :key="data">
+      <li v-for="(data, key) in sort_data" :key="data">
         {{key}}さん <v-progress-linear color="light-blue" height="30" :value="data" striped>{{data}}</v-progress-linear>
       </li>
     </ol>
@@ -14,9 +14,9 @@
     data() {
       return {
         datas:{
+            c : 80,
             a : 100,
             b : 90,
-            c : 80,
             d : 70,
             e : 60,
             f : 50,
@@ -29,7 +29,15 @@
       }
     },
     computed:{
-
+      sort_data : function(){
+        var obj=this.datas
+        var array = Object.keys(obj).map((k)=>({ key: k, value: obj[k] }));
+        array.sort((a, b) =>  - b.value-a.value);
+        obj = Object.assign({}, ...array.map((item) => ({
+          [item.key]: item.value,
+        })));
+        return obj
+      }
     }
   }
 </script>
