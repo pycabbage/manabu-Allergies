c11760cb68666bb57901715b88ced1e31c05f72e
Marusoftware <marusoftware@outlook.jp>
2021-08-05 11:02:46 +0900

    見た目をよくして、誤ったリンクを削除しました。

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,34 @@
 <template>
   <v-app id="inspire">
-    ここにページを書く
-    <center>
-      <nuxt-link to="/ranking">
-        ランキング
-      </nuxt-link>
-      <nuxt-link to="/account-setting">
-        アカウント設定
-      </nuxt-link>
-      <nuxt-link to="/mypage">
-        マイページ
-      </nuxt-link>
-    </center>
+    <!-- ここにページを書く -->
+    <v-row justify="left">
+      <v-card tile>
+        <v-list shaped>
+          <v-subheader>リンク集</v-subheader>
+          <v-list-item-group color="primary">
+            <v-list-item v-for="(item, index) in items" :key="index" :href="item">
+              <v-list-item-content>
+                <v-list-item-title v-text="index"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-card>
+    </v-row>
   </v-app>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        items : {
+          ランキング : "/ranking",
+          アカウント設定 : "/account-setting",
+          マイページ : "/my-page",
+          ダイアログのテスト : "/dialog-test"
+        }
+      }
+    }
   }
 </script>
