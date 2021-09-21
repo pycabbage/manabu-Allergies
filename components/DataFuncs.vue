b89ffde024dc79b5d2a7e35e8baf553cb5e19516
hanatsu-1404 <m20028@g.metro-cit.ac.jp>
2021-09-21 17:45:04 +0900

    firebaseのデータを使うために試行錯誤中

---
+++ components/DataFuncs.vue
@@ -0,0 +1,102 @@
+<script>
+export default {
+  computed: {
+    getEmailVerified: function () {
+      return this.$store.getters["auth/emailVerified"];
+    },
+    getId: function () {
+      return this.$store.getters["auth/id"];
+    },
+    getEmail: function () {
+      return this.$store.getters["auth/email"];
+    },
+    getName: function () {
+      return this.$store.getters["auth/name"];
+    },
+    getPhoto: function () {
+      return this.$store.getters["auth/photo"];
+    },
+  },
+  methods: {
+    logout() {
+      this.$store
+        .dispatch({
+          type: "auth/signOut",
+        })
+        .then(() => {
+          this.$router.push("/login");
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    },
+    updateProfilePhoto(e) {
+      this.$store
+        .dispatch({
+          type: "auth/updateProfile",
+          photo: e.target.files[0],
+        })
+        .then(() => {})
+        .catch((error) => {
+          alert(error);
+        });
+    },
+    updateProfileName() {
+      this.$store
+        .dispatch({
+          type: "auth/updateProfile",
+          name: this.name,
+        })
+        .then(() => {
+          this.name = "";
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    },
+    updateEmail() {
+      this.$store
+        .dispatch({
+          type: "auth/updateEmailWithAuth",
+          email: this.email,
+          confirmationPassword: this.confirmationPassword,
+        })
+        .then(() => {
+          this.email = "";
+          this.confirmationPassword = "";
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    },
+    updatePassword() {
+      this.$store
+        .dispatch({
+          type: "auth/updatePasswordWithAuth",
+          password: this.password,
+          confirmationPassword: this.confirmationPassword,
+        })
+        .then(() => {
+          this.password = "";
+          this.confirmationPassword = "";
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    },
+    deleteAccount() {
+      this.$store
+        .dispatch({
+          type: "auth/deleteAccountWithAuth",
+          confirmationPassword: this.confirmationPassword,
+        })
+        .then(() => {
+          this.confirmationPassword = "";
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    },
+  },
+};
+</script>
---
+++ middleware/redirect.js
@@ -0,0 +1,15 @@
+export default function (context) {
+  context.store.dispatch({
+    type: "auth/authStateWithVerification",
+  }).then((result) => {
+    if ((context.route.path == "/login" || context.route.path == "/create") && result.emailVerified) {
+      return context.redirect("/")
+    } else if (!(context.route.path == "/login" || context.route.path == "/create") && !result.emailVerified) {
+      return context.redirect("/login")
+    }
+  }).catch(() => {
+    if (context.route.path !== "/login" && context.route.path !== "/create") {
+      return context.redirect("/login")
+    }
+  })
+}
