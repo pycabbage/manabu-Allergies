f14c13bd99434cb5deed1c9403765ef522f631cb
tknk0369 <tknk0369@users.noreply.github.com>
2021-09-23 18:08:45 +0900

    高階関数

--- components/MyPageContent.vue
+++ components/MyPageContent.vue
@@ -1,23 +1,25 @@
 <template>
   <div>
-    {{ valueName }}:  {{ currentValue }}
+    {{ valueName }}: {{ currentValue }}
     <div class="OpenSetting">
-      <SettingDialog DialogTitle="User Setting"
-                     :ToolBarColor="ThemeColor">
-      <v-card-text class="TextareaWrapper">
-        <v-form v-model="formValid"
-                ref="form">
-          <v-text-field v-model="NewValue"
-                        :label="'新しい ' + valueName"
-                        :rules="inputValidation"/>
-        </v-form>
-      </v-card-text>
-      <v-btn  class="UpdateConfigBtn"
-              @click="updateFunc(NewValue)"
-              :color="ThemeColor"
-              :disabled="!formValid">
-        UPDATE
-      </v-btn>
+      <SettingDialog DialogTitle="User Setting" :ToolBarColor="ThemeColor">
+        <v-card-text class="TextareaWrapper">
+          <v-form v-model="formValid" ref="form">
+            <v-text-field
+              v-model="NewValue"
+              :label="'新しい ' + valueName"
+              :rules="inputValidation"
+            />
+          </v-form>
+        </v-card-text>
+        <v-btn
+          class="UpdateConfigBtn"
+          @click="updateFunc(NewValue)"
+          :color="ThemeColor"
+          :disabled="!formValid"
+        >
+          UPDATE
+        </v-btn>
       </SettingDialog>
     </div>
   </div>
@@ -55,14 +57,105 @@ export default {
   },
   methods: {
     updateValue(NewVal) {
-      if(NewVal.length < 1) {
-        this.updateFunc('default');
-      }else {
+      if (NewVal.length < 1) {
+        this.updateFunc("default");
+      } else {
         this.updateFunc(NewVal);
       }
     },
+    logout() {
+      return function () {
+        this.$store
+          .dispatch({
+            type: "auth/signOut",
+          })
+          .then(() => {
+            this.$router.push("/login");
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      };
+    },
+    updateProfilePhoto(e) {
+      return function () {
+        this.$store
+          .dispatch({
+            type: "auth/updateProfile",
+            photo: e.target.files[0],
+          })
+          .then(() => {})
+          .catch((error) => {
+            alert(error);
+          });
+      };
+    },
+    updateProfileName(name) {
+      return function () {
+        this.$store
+          .dispatch({
+            type: "auth/updateProfile",
+            name: name,
+          })
+          .then(() => {
+            //this.name = "";
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      };
+    },
+    updateEmail() {
+      return function (email, confirmationPassword) {
+        this.$store
+          .dispatch({
+            type: "auth/updateEmailWithAuth",
+            email: email,
+            confirmationPassword: confirmationPassword,
+          })
+          .then(() => {
+            // this.email = "";
+            // this.confirmationPassword = "";
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      };
+    },
+    updatePassword(password, confirmationPassword) {
+      return function () {
+        this.$store
+          .dispatch({
+            type: "auth/updatePasswordWithAuth",
+            password: password,
+            confirmationPassword: confirmationPassword,
+          })
+          .then(() => {
+            // this.password = "";
+            // this.confirmationPassword = "";
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      };
+    },
+    deleteAccount() {
+      return function (confirmationPassword) {
+        this.$store
+          .dispatch({
+            type: "auth/deleteAccountWithAuth",
+            confirmationPassword: confirmationPassword,
+          })
+          .then(() => {
+            //this.confirmationPassword = "";
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      };
+    },
   },
-}
+};
 </script>
 
 <style>
