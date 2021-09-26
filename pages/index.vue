bd926c0d8587afe17d07ced4dbb6e94f0bd22ae6
hanatsu-1404 <m20028@g.metro-cit.ac.jp>
2021-09-26 21:51:33 +0900

    トップページ作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,104 +1,89 @@
 <template>
-  <v-app id="inspire">
-    <v-stepper v-model="e1">
-      <v-stepper-header>
-        <v-stepper-step
-          :complete="e1 > 1"
-          step="1"
-        >
-          Introduction
-        </v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step
-          :complete="e1 > 2"
-          step="2"
-        >
-          How to...
-        </v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step step="3">
-          More Features
-        </v-stepper-step>
-      </v-stepper-header>
-      <v-stepper-items>
-        <v-stepper-content step="1">
-          <v-card
-            class="mb-12"
-            color="grey lighten-1"
-            height="100px"
-          >
-          <v-list-item three-line>
-            <v-list-item-content>
-              <v-list-item-title class="text-h5 mb-1">マナブアレルギーへようこそ。</v-list-item-title>
-              <v-list-item-subtitle>これは、マナブアレルギーのゲテ...素晴らしいアイコンです。</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          </v-card>
-          <SvgIcon />
-          <v-btn
-            color="primary"
-            @click="e1 = 2"
-          >
-            Continue
-          </v-btn>
-
-          <v-btn text>
-            Cancel
-          </v-btn>
-        </v-stepper-content>
-
-        <v-stepper-content step="2">
-          <v-card
-            class="mb-12"
-            color="grey lighten-1"
-            height="100px"
-          ></v-card>
-
-          <v-btn
-            color="primary"
-            @click="e1 = 3"
-          >
-            Continue
-          </v-btn>
-
-          <v-btn text>
-            Cancel
-          </v-btn>
-        </v-stepper-content>
-
-        <v-stepper-content step="3">
-          <v-card
-            class="mb-12"
-            color="grey lighten-1"
-            height="100px"
-          ></v-card>
-
-          <v-btn
-            color="primary"
-            @click="e1 = 1"
-          >
-            Continue
-          </v-btn>
-
-          <v-btn text>
-            Cancel
-          </v-btn>
-        </v-stepper-content>
-      </v-stepper-items>
-    </v-stepper>
-  </v-app>
+  <v-container>
+    <v-row justify="center">
+      <v-img src="icon.png" 
+             height="45vw"
+             contain/>
+    </v-row>
+    <v-row justify="center">
+      <p class="TopTitle">マナブアレルギーへようこそ！</p>
+    </v-row>
+    <div class="PageBtns">
+      <v-row justify="center">
+        <v-col cols="6">
+          <div class="BattleBtn">
+            <NuxtLink style="text-decoration:none;"
+                      to="battle">
+            <v-card width="100%"
+                    height="10vw"
+                    color="#ff3f49">
+              <v-icon class="mt-2"
+                      color="#151311"
+                      size="5vw">mdi-sword</v-icon>
+              <p class="BattleBtnText">クイズでバトル！</p>
+            </v-card>
+            </NuxtLink>
+          </div>
+        </v-col>
+        <v-col cols="6">
+          <div class="ScanBtn">
+            <NuxtLink style="text-decoration:none;"
+                      to="scan">
+              <v-card width="100%"
+                      height="10vw"
+                      color="#00ff6b">
+                <v-icon class="mt-2"
+                        color="#151311"
+                        size="5vw">mdi-magnify</v-icon>
+                <p class="ScanBtnText">スキャンして探す！</p>
+              </v-card>
+            </NuxtLink>
+          </div>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      e1: 1
-    };
-  },
 }
 </script>
+
+<style>
+.TopImg {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.TopTitle {
+  font-size: 5vw;
+  font-weight: bold;
+}
+
+.PageBtns {
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.BattleBtn {
+  width: 100%;
+  text-align: center;
+}
+
+.ScanBtn {
+  width: 100%;
+  text-align: center;
+}
+
+.BattleBtnText {
+  font-size: 2vw;
+  font-weight: bold;
+}
+
+.ScanBtnText {
+  font-size: 2vw;
+  font-weight: bold;
+}
+</style>
