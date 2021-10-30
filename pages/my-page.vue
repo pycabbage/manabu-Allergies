49f9fa65b6d44d943d0b89155326f5d3dd7842f7
Marusoftware <marusoftware@outlook.jp>
2021-10-30 12:11:15 +0900

    ちょっとみやすく...?

--- pages/my-page.vue
+++ pages/my-page.vue
@@ -10,20 +10,23 @@
     <v-row>
       <v-container class="Section">
         <h3>ユーザー情報</h3>
-        <v-divider />
+        <v-divider style="border-color:black"/>
         <div class="SectionContent">
           <v-form ref="nameform">
           <TextForm title="アカウント名" icon="mdi-account" editSwitch required :defaultValue="this.$store.getters['auth/name']" @callback:changed="updateProfileName" ref="name" />
           </v-form>
+          <v-divider />
           <v-form ref="mailform">
           <TextForm title="メールアドレス" editSwitch required mail :defaultValue="$store.getters['auth/email']" @callback:changed="updateEmail" @callback:pencil="show_vpasswd1=true" @callback:close="show_vpasswd1=false" ref="mail" />
           <TextForm title="パスワード" required password ref="verifyPassword1" v-show="show_vpasswd1" />
           </v-form>
+          <v-divider />
           <v-form ref="passwdform">
           <TextForm title="現在のパスワード" editSwitch required password defaultValue="**********" @callback:changed="updatePassword" @callback:pencil="show_vpasswd2=true" @callback:close="show_vpasswd2=false" ref="password" />
           <TextForm title="新しいパスワード" required password ref="verifyPassword2" v-show="show_vpasswd2" />
           <TextForm title="新しいパスワード(確認用)" required password ref="verifyPassword3" v-show="show_vpasswd2" />
           </v-form>
+          <v-divider />
           <p>ユーザーID： {{ getId }}</p>
         </div>
       </v-container>
