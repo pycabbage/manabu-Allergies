3881027cad5977bfbddf650427f5e3a09add3aca
Marusoftware <marusoftware@outlook.jp>
2021-11-02 14:26:03 +0900

    右上のアイコンが非ログイン時にログインアイコンに変わるよう変更

--- components/JumpMyPageIcon.vue
+++ components/JumpMyPageIcon.vue
@@ -1,12 +1,19 @@
 <template>
   <NuxtLink to="/my-page">
     <AvatarIcon :userIconPath="userIconPath"
-                :avatarIconSize="avatarIconSize" />
+                :avatarIconSize="avatarIconSize" 
+                v-if="getName!=''"/>
+    <v-icon :size="avatarIconSize" v-else>mdi-login</v-icon>
   </NuxtLink>
 </template>
 
 <script>
+import DataFunc from "/components/DataFuncs.vue"
 export default {
+  components:{ DataFunc },
+  computed:{
+    getName:DataFunc.computed.getName
+  },
   props: {
     userIconPath: {
       require: false,
